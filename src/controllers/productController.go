4daf82f56ff2756c13288287793e7950147aa795
axelmaker <axelmaker@ya.ru>
2022-04-05 23:11:32 +0300

    Add Redis, caching, search, sort products

--- src/controllers/productController.go
+++ src/controllers/productController.go
@@ -3,7 +3,12 @@ package controllers
 import (
 	"ambassador/src/database"
 	"ambassador/src/models"
+	"context"
+	"encoding/json"
+	"sort"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -64,4 +69,81 @@ func DeleteProduct(ctx *fiber.Ctx) error {
 	database.DB.Delete(&product)
 
 	return nil
+}
+
+func ProductsFrontend(ctx *fiber.Ctx) error {
+	var products []models.Product
+	var c = context.Background()
+
+	result, err := database.Cache.Get(c, "products_frontend").Result()
+
+	if err != nil {
+		database.DB.Find(&products)
+
+		bytes, err := json.Marshal(products)
+		if err != nil {
+			panic(err)
+		}
+
+		if errKey := database.Cache.Set(c, "products_frontend", bytes, 30*time.Minute).Err(); errKey != nil {
+			panic(errKey)
+		}
+	} else {
+		json.Unmarshal([]byte(result), &products)
+	}
+
+	return ctx.JSON(products)
+}
+
+func ProductsBackend(ctx *fiber.Ctx) error {
+	var products []models.Product
+	var c = context.Background()
+
+	result, err := database.Cache.Get(c, "products_backend").Result()
+
+	if err != nil {
+		database.DB.Find(&products)
+
+		bytes, err := json.Marshal(products)
+		if err != nil {
+			panic(err)
+		}
+
+		if errKey := database.Cache.Set(c, "products_backend", bytes, 30*time.Minute).Err(); errKey != nil {
+			panic(errKey)
+		}
+	} else {
+		json.Unmarshal([]byte(result), &products)
+	}
+
+	// search
+	var searchedProducts []models.Product
+
+	if s := ctx.Query("s"); s != "" {
+		lower := strings.ToLower(s)
+		for _, product := range products {
+			if strings.Contains(strings.ToLower(product.Title), lower) ||
+				strings.Contains(strings.ToLower(product.Description), lower) {
+				searchedProducts = append(searchedProducts, product)
+			}
+		}
+	} else {
+		searchedProducts = products
+	}
+
+	// sorted
+	if sortParam := ctx.Query("sort"); sortParam != "" {
+		sortLower := strings.ToLower(sortParam)
+		if sortLower == "asc" {
+			sort.Slice(searchedProducts, func(i, j int) bool {
+				return searchedProducts[i].Price < searchedProducts[j].Price
+			})
+		} else if sortLower == "desc" {
+			sort.Slice(searchedProducts, func(i, j int) bool {
+				return searchedProducts[i].Price > searchedProducts[j].Price
+			})
+		}
+	}
+
+	return ctx.JSON(searchedProducts)
 }
