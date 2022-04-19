375d6b8dcf83583c63aaff5969ac87db7292f0cf
axelmaker <axelmaker@ya.ru>
2022-04-19 23:35:29 +0300

    [React Admin] Add Users List

--- react-admin/src/components/Layout.tsx
+++ react-admin/src/components/Layout.tsx
@@ -1,19 +1,21 @@
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Redirect } from 'react-router-dom'
+import { User } from '../models/user'
 import Menu from './Menu'
 import Nav from './Nav'
 
+
 const Layout = (props: any) => {
     const [redirect, setRedirect] = useState(false);
+    const [user, setUser] = useState<User | null>(null);
 
     useEffect(() => {
         (
             async () => {
                 try {
                     const { data } = await axios.get('user')
-
-                    console.log(data)
+                    setUser(data)
                 } catch (e) {
                     setRedirect(true)
                 }
@@ -27,7 +29,7 @@ const Layout = (props: any) => {
 
     return (
         <div>
-            <Nav />
+            <Nav user={user} />
             <div className="container-fluid">
                 <div className="row">
                     <Menu />
--- react-admin/src/pages/Users.tsx
+++ react-admin/src/pages/Users.tsx
@@ -1,28 +1,46 @@
-import React from 'react'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Layout from '../components/Layout'
+import { User } from '../models/user';
 
 
 const Users = () => {
+    const [users, setUsers] = useState<User[]>([])
+
+    let ambassadorsUrl = 'ambassadors'
+
+    useEffect(() => {
+        (
+            async () => {
+                const { data } = await axios.get(ambassadorsUrl)
+                setUsers(data)
+            }
+        )()
+    }, [])
+
     return (
         <Layout>
             <table className="table table-striped table-sm">
                 <thead>
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1,001</td>
-                        <td>random</td>
-                        <td>data</td>
-                        <td>placeholder</td>
-                        <td>text</td>
-                    </tr>
+                    {users.map(user => {
+                        return (
+                            <tr>
+                                <td>{user.id}</td>
+                                <td>{user.first_name} {user.last_name}</td>
+                                <td>{user.email}</td>
+                                <td></td>
+                            </tr>
+                        )
+                    })}
+
                 </tbody>
             </table>
         </Layout>
